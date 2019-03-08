a3708f2b237d787f47c4237b6e294fbe2baeb58e
jeduFlash <jhonatansix>
2019-03-07 22:47:49 -0500

    ConfigFirebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyDXQOuIKQOxrysM9Jy2NwM_qc18m1qNW_4",
+    authDomain: "angularbeta.firebaseapp.com",
+    databaseURL: "https://angularbeta.firebaseio.com",
+    projectId: "angularbeta",
+    storageBucket: "angularbeta.appspot.com",
+    messagingSenderId: "381410892494"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig :{
+    apiKey: "AIzaSyDXQOuIKQOxrysM9Jy2NwM_qc18m1qNW_4",
+    authDomain: "angularbeta.firebaseapp.com",
+    databaseURL: "https://angularbeta.firebaseio.com",
+    projectId: "angularbeta",
+    storageBucket: "angularbeta.appspot.com",
+    messagingSenderId: "381410892494"
+  }
 };
 
 /*
